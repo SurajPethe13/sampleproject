c8c20912bf4c39a25c443b290d6b4589c7306bf6
Suraj Pethe <surajpethe13@gmail.com>
2023-07-13 22:55:58 +0530

    sample project

---
+++ src/samplepackage/Sample2Class.java
@@ -0,0 +1,9 @@
+package samplepackage;
+
+public class Sample2Class {
+
+	public static void main(String[] args) {
+		System.out.println("Sample 2 Class");
+		System.out.println("Sample 2 Class");
+	}
+}
---
+++ src/samplepackage/SampleClass.java
@@ -0,0 +1,10 @@
+package samplepackage;
+
+public class SampleClass {
+
+	public static void main(String[] args) {
+		System.out.println("WelCome");
+		
+		System.out.println("WelCome");
+	}
+}
